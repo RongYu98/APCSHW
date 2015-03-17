91d0cfd50c6b9eeb648dd20c2055c498bc6db12c
RongYu <stuy.rong.yu@gmail.com>
2015-03-17 09:25:06 -0400

    hw

---
+++ 08-lists/lists.java
@@ -0,0 +1,64 @@
+public class lists{
+    private Node l=null;
+    private int length;
+    private final Node nothing=null;
+    
+    public lists(){
+	l = new Node("Dummy");
+	length = 0;
+    }
+    
+    public void add(String s){
+	Node tmp = new Node(s);
+	if (length==0){
+	    l.setNext(tmp);
+	} else {
+	    tmp.setNext(l.getNext());
+	    l.setNext(tmp);
+	}
+	length++;
+    }
+		
+    public String toString(){
+	String s = "";
+	Node tmp;;
+	for (tmp=l ; tmp!=null ; tmp=tmp.getNext()){
+	    s = s + tmp + " --> ";
+	}
+	s = s + "null";
+	return s;
+    }
+
+    //PreCondition: has to be within bounds
+    public Node find(int n){
+	if (n>length){
+	    return nothing;
+	}
+	Node tmp = l.getNext();
+	for (int i=0; i<n; i++){
+	    tmp=tmp.getNext();
+	}
+	return tmp;
+    }
+
+    public void insert(int n, String S){
+	Node temp;
+	if (n==0){
+	    add(S);
+	} else if (n>length){
+	    System.out.println("OutOfBounds");
+	} else {
+	    Node tmp = l.getNext;
+	    for (int i=0; i<n-1; i++){
+		tmp=tmp.getNext();
+	    }
+	    Node tmp2=tmp.getNext();
+	    Node Newie = new Node(S);
+	    Newie.setNext(tmp.getNext());
+	    tmp.setNext(Newie);
+	    length++;
+	}
+    }
+
+    public void remove(int n){}
+}
