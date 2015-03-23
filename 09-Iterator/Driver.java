64e5da87be4b928cc41e52ba79e3f1dc25dcc8fb
Rong Yu <Stuy.rong.yu@gmail.com>
2015-03-23 18:23:49 -0400

    ..

--- 09-Iterator/Driver.java
+++ 09-Iterator/Driver.java
@@ -12,7 +12,6 @@ public class Driver{
 				//ArrayList<Integer> l2 = new ArrayList<Integer>();
 				//LinkedList<Integer> l2 = new LinkedList<Integer>();
 
-
 				l.add("Hello");
 				l.add("world");
 				l.add("abc");
@@ -21,34 +20,29 @@ public class Driver{
 
 				System.out.println(l);
 
+				System.out.println("-------------------");
+
+				/*
 				for (int i=0;i < 5; i++){
 						l2.add(i);
 				}
 
 				System.out.println(l2);
+				*/
 
+				
 				Iterator<String> i = l.iterator();
-				while (i.hasNext())
+				while (i.hasNext()){
 							 System.out.println(i.next());
+							i.remove();
+							System.out.println(l);
+				}
+				/*
 				
 				Iterator<Integer> i2 = l2.iterator();
 				while (i2.hasNext())
 							 System.out.println(i2.next());
-				
-				System.out.println("-----------------");
 
-				// Java foreach or for in loop
-
-				for (String s : l){
-						System.out.print(s+", ");
-				}
-				System.out.println();
-
-				for (int j : l2){
-						System.out.print(j+", ");
-				}
-				System.out.println();
-				
-				
+				*/
 		}
 }
--- 09-Iterator/LLit.java
+++ 09-Iterator/LLit.java
@@ -2,27 +2,36 @@ import java.io.*;
 import java.util.*;
 
 public class LLit<E> implements Iterator<E>{
+
     private Node<E> PrePrevious;
     private Node<E> Previous;
+    private Node<E> NowNode;
     public LLit(Node<E> n){
-	PrePrevious = n; //this will be predummy node
-	Previous = n; //the t will be the dummy node
-	PrePrevious.setNext(Previous);
-	Previous.setNext(n);
+	//PrePrevious = new Node("PrePre"); 
+	Previous = new Node("Pre"); 
+	NowNode=new Node("dummy");
+	//PrePrevious.setNext(Previous);
+	Previous.setNext(NowNode);
+	NowNode.setNext(n);
     }
 
     public boolean hasNext(){
-	return t.getNext()!=null;
+	return NowNode.getNext().getData()!=null;
     }
 
     public E next(){
-	E retval = t.getNext().getData();
-	t=t.getNext();
+	E retval = NowNode.getNext().getData();
+	NowNode=NowNode.getNext();
+	if (NowNode.getData()!=Previous.getNext().getData()){
+	    Previous = Previous.getNext();}
+	//Previous=NowNode;
 	return retval;
     }
 
     public void remove(){
-	// to be completed for homework
+	//PrePrevious.setNext(Previous.getNext());
+	Previous = NowNode;
     }
+
 }
 
