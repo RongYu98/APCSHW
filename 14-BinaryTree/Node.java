323c2888dc8b10093b6492de1d627d478a2d8bd3
RongYu <stuy.rong.yu@gmail.com>
2015-04-23 09:26:36 -0400

    work

---
+++ 14-BinaryTree/Node.java
@@ -0,0 +1,31 @@
+public class Node {
+    private int data;
+    private Node left=null;
+    private Node right=null;
+		
+    public Node(int data){
+	this.data=data;
+    }
+		
+    public int getData() {
+	return data;
+    }
+
+    public Node getRight() {
+	return right;
+    }
+
+    public Node getLeft(){
+	return left;
+    }
+}
+
+
+
+
+
+
+
+
+
+
