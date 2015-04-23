22cdbe1adf44271423dea5570ba8d8b4d0b05bb8
Rong Yu <Stuy.rong.yu@gmail.com>
2015-04-23 18:00:45 -0400

    homework

--- 14-BinaryTree/Tree.java
+++ 14-BinaryTree/Tree.java
@@ -1,4 +1,4 @@
-public class Tree{
+﻿public class Tree{
 
     private Node root=null;
 
@@ -17,6 +17,28 @@ public class Tree{
 	return null;
     }
 
+    public Node Search2 (int i){
+	Node T = root;
+	while (T!=null){
+	    //int c = T.getData().compareTo(i);
+	    int c = i - T.getData();
+	    if (c==0){
+		return T;}
+	    else if (c>0){
+		if (T.getRight()==null){
+		    return T;}
+		else{
+		    T=T.getRight();}
+	    }else {
+		if (T.getLeft()==null){
+		    return T;}
+		else {
+		    T=T.getLeft();}
+	    }
+	}
+	return T;
+    }
+
     public void Insert(int i){
 	Node n = new Node(i);
 	Node T=root;
@@ -24,7 +46,15 @@ public class Tree{
 		T=n;
 		return;}
 	else{
-		//insert as a new leaf
+		Node In = new Node(i);
+		Node T2 = Search2(i);
+		int c = i - T2.getData();
+		 if (c==0){
+			return;}
+		    else if (c>0){
+			T.setRight(In);}
+	    	else {
+			T=T.setLeft(In);}		
 	    /*
 	      1. Set T to root
 	      2. search for n, but use a piggyback pointer T2 to follow T.
