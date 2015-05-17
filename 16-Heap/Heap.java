fc33c324ada8e0096a977595007e4158808b7f3d
Rong Yu <Stuy.rong.yu@gmail.com>
2015-05-17 19:42:39 -0400

    homework

--- 16-Heap/Heap.java
+++ 16-Heap/Heap.java
@@ -4,7 +4,7 @@ public class Heap{
     public int[] heap;
     
     public Heap(int n){
-	heap = new int[2];
+	heap = new int[1];
 	heap[0]=n;
     }
 
@@ -12,31 +12,99 @@ public class Heap{
 	return heap[0];
     }
 
-    public void pushDown(int n){ //the number in the array; needs to be rewritten
-	while (n.getData() > n.getLeft().getData() || n.getData() > n.getRight().getData()){
-	    Node tempL = n.getLeft();
-	    Node tempR = n.getLeft();
-	    if (tempL.getData() < tempR.getData()){
-		Node temp = n;
-		n = tempL;
-		tempL = temp;
+    public void pushDown(int n){
+	if (n*2+1>heap.length-1){
+	    return;}
+	while (heap[n] > heap[2*n+1] || (2*n+2 <heap.length -1 && heap[n] > heap[2*n+2])){
+	    if (2*n+2 < heap.length-1){ // if 2*n+2 exists
+		if (heap[2*n+1] < heap[2*n+2]){
+		    int temp = heap[n];
+		    heap[n] = heap[2*n+1];
+		    heap[2*n+1] = temp;
+		    n = 2*n+1;
+		    if (n*2+1>heap.length-1){
+	    		return;}
+		} else {
+		    int temp = heap[n];
+		    heap[n] = heap[2*n+2];
+		    heap[2*n+2] = temp;
+		    n = 2*n + 2;
+		    if (n*2+2>heap.length-1){
+	    		return;}
+	   	}
 	    } else {
-		Node temp = n;
-		n = tempR;
-		tempR = temp;
+		int temp = heap[n];
+		heap[n] = heap[2*n+1];
+		heap[2*n+1] = temp;
+		n = 2*n + 1;
+		if (n*2+1>heap.length-1){
+	    		return;}
 	    }
 	}
     }
 	    
     public void removeMin(){
 	heap[0]=heap[heap.length-1];
-	
+	int[] temp = new int[heap.length-1];
+	for (int i=0; i<temp.length; i++){
+	    temp[i]=heap[i];}
+	heap = temp;
+	pushDown(0);
     }
 
-    public void pushUp(int n){
+    public void shiftUp(int n){
+	while (n!=0){
+	    if (n%2==0){
+ 	    	if (heap[n] < heap[n/2-1]){
+		    int temp = heap[n];
+		    heap[n] = heap[n/2-1];
+		    heap[n/2-1] = temp;
+		    n = n/2-1;
+		} else {
+		    return;
+	    	}
+	    } else if (heap[n] < heap[n/2]){
+		int temp = heap[n];
+		heap[n] = heap[n/2];
+		heap[n/2] = temp;
+		n = n/2;
+	    } else {
+		return;
+	    }
+	}
     }
 
-    public void heapSort(){
+    public void insert(int n){
+	int[] temp = new int[heap.length+1];
+	for (int i=0; i<heap.length; i++){
+	    temp[i] = heap[i];}
+	temp[temp.length-1] = n;
+	heap = temp;
+	shiftUp(heap.length-1);
+    }
+
+    public int[] heapSort(){
+	int[] temp = new int[heap.length];
+	for (int i = 0; i< heap.length; i++){
+	    temp[i]=heap[i];
+	}
+	int[] ret = new int[heap.length];
+	for (int i=0; i<heap.length; i++){
+	    ret[i] = findMin();
+	    removeMin();}
+	heap = temp;
+	return ret;
+    }
+
+    public static void main(String[] args){
+	Heap h = new Heap(0);
+	for (int i = 0; i< 10; i++){
+	    h.insert(i);
+	}
+	int[] temp = h.heapSort();
+	for (int i=0; i<temp.length; i++){
+	    System.out.print(i+"   ");}
+	System.out.println();
     }
 
 }
